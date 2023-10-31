ece2a2ff53d4cf453374b3af5e8aea3f95c0d51d
Hannes Ebner <hannes@metasolutions.se>
2023-10-31 17:54:43 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+rdforms.spec({
+  language: 'de',
+  bundles: [
+    ['../dcatapat.json'],
+  ],
+  main: [
+    'dcatat:OnlyCatalog'
+  ],
+  language: document.targetLanguage,
+  supportive: [
+  ]
+});
---
+++ libs/recommendations.js
@@ -0,0 +1,57 @@
+var lang2label = {
+  'sv': 'Rekommendation',
+  'en': 'Recommendation',
+  'de': 'Empfehlung'
+};
+
+window.addRecommendations = function(lang, recs) {
+  const refs = {};
+  for (var key in recs) if (recs.hasOwnProperty(key)) {
+    var ids = recs[key];
+    for (var i = 0; i < ids.length; i++) {
+      var id = ids[i];
+      refs[id] = refs[id] || [];
+      refs[id].push(key);
+    }
+  }
+
+  for (var anchor in refs) if (refs.hasOwnProperty(anchor)) {
+    var targets = refs[anchor];
+    try {
+      var a = document.getElementById(anchor);
+      var tbody = a.nextElementSibling.firstElementChild;
+      var tr = document.createElement('tr');
+      var th = document.createElement('th');
+      th.innerText = lang2label[document.targetLanguage];
+      tr.appendChild(th);
+      var td = document.createElement('td');
+      tr.appendChild(td);
+      tbody.appendChild(tr);
+
+      for (var j = 0; j < targets.length; j++) {
+        var target = targets[j];
+        var textLinkArr = target.split('-');
+        var textLink = textLinkArr[0] + '. ';
+        textLinkArr.shift();
+        textLink += textLinkArr[0].substr(0, 1).toUpperCase() + textLinkArr[0].substr(1) + ' ';
+        textLinkArr.shift();
+        textLink += textLinkArr.join(' ');
+        // Remove åäö from link
+        target = target.replace(/å/g,'a');
+        target = target.replace(/ä/g,'a');
+        target = target.replace(/ö/g,'o');
+
+        var recLink = document.createElement('a');
+        recLink.innerText = textLink;
+        recLink.href = '#' + target;
+        td.appendChild(recLink);
+        if ((j + 1) < targets.length) {
+          var separator = document.createTextNode(', ');
+          td.appendChild(separator);
+        }
+      }
+    } catch (e) {
+      console.log("Failed create recommendation link from " + id + " to " + key);
+    }
+  }
+};
